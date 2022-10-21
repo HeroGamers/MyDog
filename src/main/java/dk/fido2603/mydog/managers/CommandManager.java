4c5ca17f1cabf558f299a46f111eb0b5695ed293
HeroGamers <hero@herogamers.dev>
2022-10-21 18:21:29 +0200

    Third times the charm

--- src/main/java/dk/fido2603/mydog/managers/CommandManager.java
+++ src/main/java/dk/fido2603/mydog/managers/CommandManager.java
@@ -4,7 +4,7 @@ import java.text.DecimalFormat;
 import java.util.*;
 
 import dk.fido2603.mydog.MyDog;
-import org.apache.commons.lang.Validate;
+import org.apache.commons.lang3.Validate;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.attribute.Attribute;
